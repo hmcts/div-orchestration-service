792fac94936d8bcf5a94d47fa47a4ecc04df204c
J. Victor Ferreira <victor.ferreira@hmcts.net>
2021-03-15 17:13:46 +0000

    First notes

---
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/event/listener/AOSOfflineTriggerListener.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.reform.divorce.orchestration.event.listener;
+
+public class AOSOfflineTriggerListener {//TODO - rethink name
+
+    //TODO - which event(s) should this trigger?
+    //events starting with "IssueAOSOffline_Respondent". they need to be unified first.
+    //Then, I need to check whether the current states cater all the possible states the case can be in after it's issued or reissued.
+
+}
